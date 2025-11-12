4807c88096bdf13cf4923a5e6db3d16891d9f871
Aaru5h <mestrikerocks@gmail.com>
2025-11-12 12:39:01 +0530

    cp

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,7 @@ const app = express();
 
 const port = 4000
 
-app.use(cors({
-  origin: ["https://https://shelfly-34bg.vercel.app/", "http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
