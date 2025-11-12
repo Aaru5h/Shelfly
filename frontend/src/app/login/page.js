ec7d0ee9de27c5f650be34f694318568d8f81073
Aaru5h <mestrikerocks@gmail.com>
2025-11-12 17:43:42 +0530

    fixed login issue

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -12,7 +12,7 @@ export default function LoginPage() {
     setMessage("");
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
